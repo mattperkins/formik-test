9a5799ee992dbd33250bb120e497b53ff2e0ce8e
mattperkins <github@mattperkins.com>
2018-10-15 20:11:43 +0100

    add yup validation and error component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import * as React from 'react'
 import { withFormik, Form, Field } from 'formik'
-import Yup from 'yup'
+import * as Yup from 'yup'
+
+
+const Erz = ({label}) => (
+  <p style={{color:'red', fontSize:'.6rem', marginBottom: 3}}>{label} must be valid</p>  
+)
 
 const App = ({
-    values
+    values,
+    errors,
+    touched
 }) => (
 
   <Form>
 
-      <Field type="email" name="email" placeholder="Email" />
-      <Field type="password" name="password" placeholder="Password" />
+<div>
+      <div> { touched.email && errors.email && <Erz label="email" /> }
+        <Field type="email" name="email" placeholder="Email" />
+      </div>
+
+      <div> { touched.password && errors.password  && <Erz label="password" /> }
+        <Field type="password" name="password" placeholder="Password" />
+      </div>
+
       <button type="submit">Submit</button>
+</div>
+
       <div>
         <Field type="checkbox" name="newsletter" checked={values.newsletter} />
         Sign up to our newsletter
       </div>
+
       <div>
         <Field component="select" name="plan">
           <option value="free">Free</option>
           <option value="premium">Premium</option>
         </Field>
       </div>
+
   </Form>
 
 )
@@ -34,6 +52,11 @@ const FormikApp = withFormik({
         plan: plan || 'free'
     }  
   },
+  // integrate Yup
+  validationSchema: Yup.object().shape({
+   email: Yup.string().email().required(),
+   password: Yup.string().min(4).required() 
+  }), // end Yup
   handleSubmit(values) {
     console.log(values) 
   }
